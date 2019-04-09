5e90063347f306a8d5d7681aa40e7bcf8c9f3f3f
MalditoBarbudo <victorgrandagarcia@gmail.com>
2019-04-09 16:13:25 +0200

    integrated flexdashboard in the package

--- R/app.R
+++ R/app.R
@@ -1,112 +1,23 @@
 #' function to launch the nfi app
 #'
+#' @param file character indicating the flexdashboard Rmd file to load
+#' @param shiny_args list with the shiny arguments for runApp
+#' @param render_params list of named parameters for the Rmd file
+#'
 #' @export
-allometr_app <- function() {
-
-  ## database ####
-  allometr_db <- pool::dbPool(
-    RPostgreSQL::PostgreSQL(),
-    user = 'guest',
-    password = 'guest',
-    dbname = 'allometr_db'
+allometr_app <- function(
+  file,
+  shiny_args = list(host = '0.0.0.0', port = 3838),
+  render_params =  list(
+    appdb_user = 'guest',
+    appdb_password = 'guest',
+    appdb_dbname = 'allometr_db',
+    appdb_host = 'localhost'
   )
+) {
 
-  ## tables ####
-  allometries_table <- dplyr::tbl(allometr_db, 'ALLOMETRIES') %>% dplyr::collect()
-  variables_thesaurus <- dplyr::tbl(allometr_db, 'THESAURUS_VARIABLES') %>% dplyr::collect()
-  cubication_thesaurus <- dplyr::tbl(allometr_db, 'THESAURUS_CUBICATION') %>% dplyr::collect()
-
-  ## UI ####
-  ui <- shiny::tagList(
-    shinyjs::useShinyjs(),
-    shiny::fluidPage(
-
-      # title
-      shiny::titlePanel('Allometries for everyone!!'),
-
-      # layout
-      shiny::sidebarLayout(
-        sidebarPanel = shiny::sidebarPanel(
-          width = 2,
-          mod_dataInput(
-            'mod_dataInput',
-            allometries_table, variables_thesaurus, cubication_thesaurus
-          )
-        ),
-
-        mainPanel = shiny::mainPanel(
-          width = 10,
-
-          ########################################################### debug ####
-          # shiny::absolutePanel(
-          #   id = 'debug', class = 'panel panel-default', fixed = TRUE,
-          #   draggable = TRUE, width = 640, height = 'auto',
-          #   # top = 100, left = 100, rigth = 'auto', bottom = 'auto',
-          #   # top = 'auto', left = 'auto', right = 100, bottom = 100,
-          #   top = 60, left = 'auto', right = 50, bottom = 'auto',
-          #
-          #   shiny::textOutput('debug1'),
-          #   shiny::textOutput('debug2'),
-          #   shiny::textOutput('debug3')
-          # ),
-          ####################################################### end debug ####
-
-          shiny::fluidRow(
-            mod_tableOutput(
-              'mod_tableOutput',
-              allometries_table, variables_thesaurus, cubication_thesaurus
-            )
-          ),
-          shiny::br(),
-          shiny::br(),
-          mod_equationOutput('mod_equationOutput')
-        )
-      )
-    )
+  # wrapper for rmarkdown::run with the corresponding arguments
+  rmarkdown::run(
+    file = file, shiny_args = shiny_args, render_args = list(params = render_params)
   )
-
-  ## SERVER ####
-  server <- function(input, output, session) {
-
-    data_reactives <- shiny::callModule(
-      mod_data, 'mod_dataInput',
-      allometries_table, variables_thesaurus, cubication_thesaurus
-    )
-
-    table_reactives <- shiny::callModule(
-      mod_table, 'mod_tableOutput',
-      allometries_table, variables_thesaurus, cubication_thesaurus,
-      data_reactives
-    )
-
-    shiny::callModule(
-      mod_equation, 'mod_equationOutput',
-      data_reactives, table_reactives
-    )
-
-    ## debug #####
-    # output$debug1 <- shiny::renderPrint({
-    #   data_reactives$allolvl
-    # })
-    # output$debug2 <- shiny::renderPrint({
-    #   data_reactives$spatial
-    # })
-    # output$debug3 <- shiny::renderPrint({
-    #   table_reactives$allometries_table_rows_selected
-    # })
-  }
-
-  # Run the application
-  allometr_app_res <- shiny::shinyApp(
-    ui = ui, server = server,
-    onStart = function() {
-
-      ## on stop routine to cloose the db pool
-      shiny::onStop(function() {
-        pool::poolClose(allometr_db)
-      })
-    }
-  )
-
-  return(allometr_app_res)
 }
