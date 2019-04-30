597330353fe16cad3f70cb6520dd074f87b6532f
MalditoBarbudo <victorgrandagarcia@gmail.com>
2019-04-30 12:39:22 +0200

    added calculated data functionality

--- R/app.R
+++ R/app.R
@@ -121,15 +121,48 @@ allometr_app <- function(
             mod_dataInput('mod_dataInput')
           ),
           mainPanel = shiny::mainPanel(
-            width = 9
+            width = 9,
+
+            # tabset panel
+            shiny::tabsetPanel(
+
+              # table tab
+              shiny::tabPanel(
+                'Table',
+                DT::DTOutput('allometr_table')
+                # download buttons
+              ),
+
+              # calculate panel
+              shiny::tabPanel(
+                'Calculate',
+
+                shiny::fluidRow(
+                  shiny::column(
+                    4,
+                    shiny::fileInput(
+                      'user_data', NULL, FALSE,
+                      accept = c(
+                        '.csv', '.xlsx', 'text/csv', 'text/comma-separated-values,text/plain',
+                        'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                        buttonLabel = 'Browse...', placeholder = 'No file selected...'
+                      )
+                    ),
+                    selectInput(
+                      'allometry_selector', NULL, choices = '', size = 5, selectize = FALSE
+                    ),
+                    uiOutput('var_declaration')
+                  ),
+                  shiny::column(
+                    8,
+                    shiny::tableOutput('res_data')
+                  )
+                )
+              )
+            )
           )
-
-
-
         )
       )
-
-
     ) # end of navbarPage
   )
 
@@ -155,8 +188,139 @@ allometr_app <- function(
     # data inputs
     data_reactives <- shiny::callModule(
       mod_data, 'mod_dataInput',
-      allometr_table, variables_thesaurus, cubication_thesaurus, lang
+      allometries_table, variables_thesaurus, cubication_thesaurus, lang
     )
+
+    ## allo table ####
+    output$allometr_table <- DT::renderDT({
+      allometries_table %>%
+        dplyr::filter(!!! data_reactives$filtering_expr) %>%
+        dplyr::mutate_if(is.numeric, round, 3) %>%
+        DT::datatable(
+          class = 'compact hover nowrap row-border order-column',
+          extensions = 'Scroller',
+          options = list(
+            dom = 'tr',
+            # pageLength = 10,
+            # lengthMenu = c(10, 25, 50),
+            deferRender = TRUE,
+            scrollY = '600px', scroller = TRUE, scrollX = TRUE
+          )
+        )
+    })
+
+    ## res table ####
+    # left side
+    user_data <- shiny::reactive({
+
+      if (is.null(input$user_data)) {
+        return(NULL)
+      }
+
+      extension <- input$user_data$name %>%
+        stringr::str_extract('\\.[a-zA-Z]{3,4}$')
+      if (extension == '.csv') {
+        res <- readr::read_csv(input$user_data$datapath)
+      } else {
+        res <- readxl::read_excel(input$user_data$datapath)
+      }
+    })
+
+    observe({
+      id_choices <- allometries_table %>%
+        dplyr::filter(!!! data_reactives$filtering_expr) %>%
+        dplyr::pull(allometry_id)
+
+      updateSelectInput(
+        session, 'allometry_selector', NULL, id_choices
+      )
+    })
+
+    output$var_declaration <- renderUI({
+
+      shiny::validate(
+        shiny::need(user_data(), 'No user data provided'),
+        shiny::need(input$allometry_selector, 'No user data provided')
+      )
+
+      allom_id <- input$allometry_selector
+      allom_desc <- allom_description(id = allom_id)
+
+      independent_vars <- c(
+        allom_desc[[allom_id]][['independent_var_1']],
+        allom_desc[[allom_id]][['independent_var_2']],
+        allom_desc[[allom_id]][['independent_var_3']]
+      ) %>%
+        purrr::discard(function(x) {is.na(x)})
+
+      lapply(independent_vars, function(x) {
+        varSelectInput(
+          glue::glue("{x}_input"),
+          glue::glue("Variable acting as {x}"),
+          # x,
+          data = user_data() %>% dplyr::select_if(is.numeric),
+          selectize = FALSE, size = 5
+        )
+      })
+    })
+
+    allom_variables_exprs <- reactive({
+
+      allom_id <- input$allometry_selector
+      allom_desc <- allom_description(id = allom_id)
+
+      independent_vars <- c(
+        allom_desc[[allom_id]][['independent_var_1']],
+        allom_desc[[allom_id]][['independent_var_2']],
+        allom_desc[[allom_id]][['independent_var_3']]
+      ) %>%
+        purrr::discard(function(x) {is.na(x)})
+
+      # TODO convert to Dn = Dn expressions
+      independent_vars %>%
+        purrr::walk(
+          ~ shiny::validate(
+            shiny::need(input[[paste0(.x, '_input')]], 'No variable declaration')
+          )
+        ) %>%
+        purrr::map_chr(
+          ~ glue::glue("{.x} = {input[[paste0(.x, '_input')]]},")
+        ) %>%
+        stringr::str_c(collapse = ' ')
+
+    })
+
+    calculated_data <- reactive({
+      shiny::validate(
+        shiny::need(user_data(), 'No user data provided'),
+        shiny::need(input$allometry_selector, 'No user data provided')
+
+        # shiny::need(
+        #   !is.character(user_data()[['']])
+        # )
+      )
+
+      shiny::validate(
+        shiny::need(allom_variables_exprs(), 'No variables declaration provided'),
+        shiny::need(length(allom_variables_exprs()) > 0, 'No variables declaration provided')
+      )
+
+      # let's try to do it with glue + parse_exprs because it will be easier I think
+      glue::glue(
+        "user_data() %>%
+           allom_calculate(
+             {paste0(allom_variables_exprs(), sep = ' ')}
+             allometry_id = '{input$allometry_selector}',
+             name = '{allom_description(id = input$allometry_selector)[[input$allometry_selector]]$dependent_var}'
+           )"
+      ) %>%
+        rlang::parse_expr() %>%
+        eval()
+    })
+
+    output$res_data <- renderTable({
+      calculated_data()
+    })
   }
 
   # Run the application
