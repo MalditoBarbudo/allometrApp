9d73e596a6e40777b93829c4edeb1a8277c43334
MalditoBarbudo <victorgrandagarcia@gmail.com>
2019-05-02 17:08:23 +0200

    new example data with more variables

--- data-raw/example_data_creation.R
+++ data-raw/example_data_creation.R
@@ -2,7 +2,11 @@
 tibble::tibble(
   tree = glue::glue("a_{1:100}") %>% as.character(),
   diametro = rnorm(100, 32, 8),
-  altura = rnorm(100, 55, 8)
+  altura = rnorm(100, 55, 8),
+  volumen_cc = rnorm(100, 23, 2),
+  diametro_rama = rnorm(100, 4, 0.2),
+  diametro_sc = diametro - rnorm(100, 1, 0.2),
+  biomasa_at = rnorm(100, 12, 1)
 ) %>% {
   test_data <- .
   readr::write_csv(test_data, 'data-raw/test_data.csv')
